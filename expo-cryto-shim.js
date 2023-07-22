a8f83e0c263c8f92ed36c5173f9b8fbf84f480e6
Richa-iitr <richa31102@gmail.com>
2023-07-22 13:16:37 +0200

    WalletConnect fix

---
+++ expo-cryto-shim.js
@@ -0,0 +1,15 @@
+import { getRandomValues as expoCryptoGetRandomValues } from "expo-crypto";
+class Crypto {
+    getRandomValues = expoCryptoGetRandomValues;
+    }
+const webCrypto = typeof crypto !== "undefined" ? crypto : new Crypto();
+
+(() => {
+    if (typeof crypto === "undefined") {
+        Object.defineProperty(window, "crypto", {
+            configurable: true,
+            enumerable: true,
+            get: () => webCrypto,
+        });
+    }
+})();
